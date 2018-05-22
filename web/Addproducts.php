0681ef6b1ead83b0996c6dcceb29a26898c395d7
Devan Weber <devanweber@gmail.com>
2018-05-22 14:55:13 -0700

    Updated weekly report for demo02, minor modifications

---
+++ web/Addproducts.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<link rel="stylesheet" href="default.css">
+<title>Add a product</title>
+</head>
+<body>
+<form action="" method="GET">
+<p>Game Name: <input type="text" name="gamename" /></p>
+<p>Description: <input type="text" name="gamedesc" /></p>
+<p>IMAGE PLACEHOLDER</p>
+<p>Price: <input type="text" name="price" /></p>
+<p>Console: 
+<input type="radio" name="PS4" value="PS4"> PS4
+<input type="radio" name="XBOX" value="XBOX"> XBOX ONE
+<input type="radio" name="PC" value="PC"> PC
+<input type="radio" name="SWITCH" value="SWITCH"> SWITCH</p>
+
+<input type="submit" value="Submit" />
+</form>
+</body>
+</html>
