49b28d50ea1004ed4ae5d3bcb02652f3b1dd3437
Pat Horler <mrphorler@live.com>
2018-05-24 14:16:18 -0700

    Added header.html, altered index.html to index.php, tasklog(incomplete)

---
+++ web/index.php
@@ -0,0 +1,43 @@
+<?php
+include('header.html');
+	
+?>
+<article>
+	<h1>Products</h1>
+	<nav class="console">
+<ul>
+	<li><a>All</a></li>
+	<li><a>Xbox One</a></li>
+	<li><a>PlayStation 4</a></li>
+	<li><a>PC</a></li>
+	<li><a>Nintendo Switch</a></li>
+	
+	
+</ul>
+</nav>
+
+<div class="textBack" align="left" style="float:left" >
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+</div>
+<br>
+<br>
+<br>
+<br>
+
+	
+
+</article>
+
+<hr>
+
+<footer>
+Copyright &copy; 2018 Curtis Naples	
+</footer>
+
+</body>
+</html>
