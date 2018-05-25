d7e35b52111095084a804490397a45f7d34f9498
Pat Horler <mrphorler@live.com>
2018-05-25 16:21:07 -0700

    Attempt 7

--- web/index.php
+++ web/index.php
@@ -2,21 +2,36 @@
 include('header.html');
 	
 ?>
-<article>
-	<h1>Products</h1>
-	<nav class="console">
-<ul>
-	<li><a>All</a></li>
-	<li><a>Xbox One</a></li>
-	<li><a>PlayStation 4</a></li>
-	<li><a>PC</a></li>
-	<li><a>Nintendo Switch</a></li>
-	
-	
-</ul>
-</nav>
 
+<br>
 <div class="textBack" align="left" style="float:left" >
+<p>
+<h1>Welcome to BiFrost Games!</h1>
+<br>
+<br>
+<br>
+<br>
+
+Lorem ipsum dolor sit amet, consectetuer adipiscing 
+elit. Aenean commodo ligula eget dolor. Aenean massa 
+<strong>strong</strong>. Cum sociis natoque penatibus 
+et magnis dis parturient montes, nascetur ridiculus 
+mus. Donec quam felis, ultricies nec, pellentesque 
+eu, pretium quis, sem. Nulla consequat massa quis 
+enim. Donec pede justo, fringilla vel, aliquet nec, 
+vulputate eget, arcu. In enim justo, rhoncus ut, 
+imperdiet a, venenatis vitae, justo. Nullam dictum 
+felis eu pede <a class="external ext" href="products.php">link</a> 
+mollis pretium. Integer tincidunt. Cras dapibus. 
+Vivamus elementum semper nisi. Aenean vulputate 
+eleifend tellus. Aenean leo ligula, porttitor eu, 
+consequat vitae, eleifend ac, enim. Aliquam lorem ante, 
+dapibus in, viverra quis, feugiat a, tellus. Phasellus 
+viverra nulla ut metus varius laoreet. Quisque rutrum. 
+Aenean imperdiet. Etiam ultricies nisi vel augue. 
+Curabitur ullamcorper ultricies nisi.
+
+</p>
 <br>
 <br>
 <br>
