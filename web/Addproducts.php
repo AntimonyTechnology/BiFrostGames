5a88a4612b8fa20b7975b91ae8ce973db21edcec
Evan <evan_24plant@hotmail.com>
2018-05-25 16:19:55 -0700

    Improved validation and added file upload (not yet saving the file to server)

--- web/Addproducts.php
+++ web/Addproducts.php
@@ -4,102 +4,94 @@
 <title>Add a product</title>
 
 <script>
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
-function validation() {
-if (document.getElementById("gamename").value.length < 1 ){
-	alert("You must include a Game Name");
-	return false;
-}
-//if (document.getElementById("gamedesc").value == ''){
-	//alert("You must include a game description");
-	//return false;
-//}
-//if (document.getElementById("image").value == ''){
-	//alert("You must include a image");
-	//return false;
-//}
-if (document.getElementById("price").value == ''){
-	alert("You must include a price");
-	return false;
-}
-//if (document.getElementById("price").value != /^\d+(\.\d{1,2})?$/ ){
-	//alert("Price can only be a number");
-	//return false;
-//}
-var priceexp = /^\d+(\.\d{1,2})?$/;
-var exptest = priceexp.test(document.getElementById("price").value);
-if(!exptest){
-	alert("Price can only be a number");
-	return false;
-}
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+    }
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+    }
+    function validation() {
+        /* Test that Game Name isn't empty
+        if (document.getElementById("gamename").value.length < 1 ){
+            alert("You must include a Game Name");
+            return false;
+        } */
 
- var inputElems = document.getElementsByTagName("input");
-      var count = 0;
-      for (var i=0; i<inputElems.length; i++) {
-        if (inputElems[i].type === "checkbox" && inputElems[i].checked === true) {
-          count++;
+        /* Test that the game has a valid price
+        if (document.getElementById("price").value == ''){
+            alert("You must include a price");
+            return false;
         }
-      }
-      if(count < 1){
-        alert("You must select a genre");
-        return false;
-      }
 
-}
+        var priceexp = /^\d+(\.\d{1,2})?$/;
+        var exptest = priceexp.test(document.getElementById("price").value);
+        if(!exptest){
+            alert("Price can only be a number");
+            return false;
+        } */
+
+        // Make sure that at least one genre is selected
+        var inputElems = document.getElementsByTagName("input");
+        var count = 0;
+        for (var i=0; i<inputElems.length; i++) {
+            if (inputElems[i].type === "checkbox" && inputElems[i].checked === true) {
+                count++;
+            }
+        }
+        if(count < 1){
+            alert("You must select at least one genre");
+            return false;
+        }
+
+    }
 </script>
 
 </head>
 <body>
-<header><img id="banner" src="BiFrostBanner.gif" alt="BiFrost Games"/></header>
-<hr>
-<nav>
-<ul id="mySidenav" class="sidenav">
-	<li><a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;&ensp;</a></li>
-	<li><a href="index.html" title="Browse products" class="viewing">Shop</a></li>	
-	<li><a href="info.html" title="View your cart" >My Cart</a></li>
-	<li><a href="signup.html" title="Sign Up here!">Sign Up</a></li>
-	<li class="icon"><a href="javascript:void(0);" style="font-size:15px;" onclick="myFunction()">&#9776;</a></li>
-</ul>
-</nav>
+    <header><img id="banner" src="BiFrostBanner.gif" alt="BiFrost Games"/></header>
+    <hr>
+    <nav>
+    <ul id="mySidenav" class="sidenav">
+        <li><a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;&ensp;</a></li>
+        <li><a href="index.html" title="Browse products" class="viewing">Shop</a></li>
+        <li><a href="info.html" title="View your cart" >My Cart</a></li>
+        <li><a href="signup.html" title="Sign Up here!">Sign Up</a></li>
+        <li class="icon"><a href="javascript:void(0);" style="font-size:15px;" onclick="myFunction()">&#9776;</a></li>
+    </ul>
+    </nav>
 
-<span style="font-size:32px;cursor:pointer" onClick="openNav()">&#9776;</span> 
+    <span style="font-size:32px;cursor:pointer" onClick="openNav()">&#9776;</span>
 
-<h1>Add Product</h1>
+    <h1>Add Product</h1>
 
-<div class="textBack" align="left" style="float:left" >
+    <div class="textBack" align="left" style="float:left" >
 
-<form action="ProductInsert.php" method="POST" enctype="multipart/form-data" onsubmit="return validation();">
-<input type="hidden" name="MAX_FILE_SIZE" value="100000">
-<p>Game Name: <input type="text" name="gamename" id="gamename" /></p>
-<p>Description: <input type="textarea" name="gamedesc" id="gamedesc" /></p>
-<p>IMAGE: <input type="text" name="image" id="image" /> </p>
-<p>Price: <input type="text" name="price" id="price" /></p>
-<p>Console: 
-<input type="radio" name="console" value="PS4" required> PS4
-<input type="radio" name="console" value="XB1" required> XBOX ONE
-<input type="radio" name="console" value="PC" required> PC
-<input type="radio" name="console" value="Nintendo_Switch" required> SWITCH</p>
+    <form action="ProductInsert.php" method="POST" enctype="multipart/form-data" onsubmit="return validation();">
+        <input type="hidden" name="MAX_FILE_SIZE" value="100000">
+        <p>Game Name: <input type="text" name="gamename" id="gamename" required /></p>
+        <p>Price: <input type="text" name="price" id="price" pattern="^\d+(\.\d{1,2})?$" required /></p>
+        <p>Description: </p><textarea name="gamedesc" id="gamedesc" rows="8" cols="60"></textarea>
+        <p>Image: <input type="file" name="image" id="image" accept="image/png, image/jpeg"/></p>
+        <p>Console:
+            <input type="radio" name="console" value="PS4" required> PS4
+            <input type="radio" name="console" value="XB1" required> XBOX ONE
+            <input type="radio" name="console" value="PC" required> PC
+            <input type="radio" name="console" value="Nintendo_Switch" required> SWITCH</p>
+        <p> Genre:
+            <input type="checkbox" name="genre[]" value="action"> Action
+            <input type="checkbox" name="genre[]" value="adventure"> Adventure
+            <input type="checkbox" name="genre[]" value="strategy"> Strategy
+            <input type="checkbox" name="genre[]" value="rpg"> RPG
+            <input type="checkbox" name="genre[]" value="MMO"> MMO
+            <input type="checkbox" name="genre[]" value="open-world"> Open-world
+            <input type="checkbox" name="genre[]" value="racing"> Racing
+            <input type="checkbox" name="genre[]" value="shooter"> Shooter</p>
+        <input type="submit" value="Submit" />
+    </form>
 
-<p> Genre:
-<input type="checkbox" name="genre[]" value="action"> Action
-<input type="checkbox" name="genre[]" value="adventure"> Adventure
-<input type="checkbox" name="genre[]" value="strategy"> Strategy
-<input type="checkbox" name="genre[]" value="rpg"> RPG
-<input type="checkbox" name="genre[]" value="MMO"> MMO
-<input type="checkbox" name="genre[]" value="open-world"> Open-world
-<input type="checkbox" name="genre[]" value="racing"> Racing
-<input type="checkbox" name="genre[]" value="shooter"> Shooter</p>
-
-<input type="submit" value="Submit" />
-</form>
-</div>
-<footer>
-Copyright &copy; 2018 Curtis Naples	
-</footer>
+    </div>
+    <footer>
+    Copyright &copy; 2018 Curtis Naples
+    </footer>
 </body>
 </html>
--- web/ProductInsert.php
+++ web/ProductInsert.php
@@ -1,40 +1,44 @@
 
 <?php
-session_start();
-include('header.html');
-echo "<h1>Add Product</h1>";
+    session_start();
+    include('header.html');
+    echo "<h1>Add Product</h1>";
 
-echo "<div class=\"textBack\" align=\"left\" style=\"float:left\">";
-$gamename = $_POST['gamename'];
-$gamedesc = $_POST['gamedesc'];
-$image = $_POST['image'];
-$price = $_POST['price'];
-$console = $_POST['console'];
-$genre = $_POST['genre'];
+    echo "<div class=\"textBack\" align=\"left\" style=\"float:left\">";
+        $gamename = strip_tags($_POST['gamename']);
+        $price = strip_tags($_POST['price']);
+        $gamedesc = nl2br(addslashes(strip_tags($_POST['gamedesc'])));
+        $console = $_POST['console'];
+        $genre = $_POST['genre'];
+        $imagename = $_FILES['image']['name'];
+        $imagetmp = $_FILES['image']['tmp_name'];
+        $imageurl = 'images/games/' . $imagename;
 
-include('connectionSQL.php');
+        echo "<p>$imagename is being uploaded.</p>";
+        move_uploaded_file($_FILES['image']['tmp_name'],"/home/student/cst160/public_html/ICS199/Project/shopping-cart/web/images/games/{$_FILES['image']['name']}");
+        echo "<p>Moving from $imagetmp to $imageurl</p>";
 
-$gamequery = "INSERT INTO games (name, description, image, price, console_name) VALUES ('$gamename', 'htmlspecialchars($gamedesc)', '$image', '$price', '$console')";
-@mysqli_query ($link, $gamequery);
+        include('connectionSQL.php');
 
-$gameidquery = "SELECT LAST_INSERT_ID()";
-$gameidresult = @mysqli_query($link, $gameidquery);
-$gameidrow = mysqli_fetch_row($gameidresult);
-$gameid = $gameidrow[0];
+        // Add the new game to the database
+        $gamequery = "INSERT INTO games (name, description, image, price, console_name) VALUES ('$gamename', '$gamedesc', '$imageurl', '$price', '$console')";
+        @mysqli_query ($link, $gamequery);
 
-echo "<p>Successfully added game # $gameid</p>";
-echo "<p><a href=\"Addproducts.php\">Add another product?</a></p>";
-echo "</div>";
-//print_r($genre);
-foreach($genre as &$value){
-	
-	
-	
-	$genrequery = "INSERT INTO game_genres values('$gameid', '$value')";
-	@mysqli_query ($link, $genrequery);
-	
-}
+        // Retrieve the game_id of the new game
+        $gameidquery = "SELECT LAST_INSERT_ID()";
+        $gameidresult = @mysqli_query($link, $gameidquery);
+        $gameidrow = mysqli_fetch_row($gameidresult);
+        $gameid = $gameidrow[0];
 
+        // Add the game genres to the database
+        foreach($genre as &$value){
+            $genrequery = "INSERT INTO game_genres values('$gameid', '$value')";
+            @mysqli_query ($link, $genrequery);
+        }
+
+        echo "<p>Successfully added game # $gameid</p>";
+        echo "<p><a href=\"Addproducts.php\">Add another product?</a></p>";
+    echo "</div>";
 ?>
 
 </body>
