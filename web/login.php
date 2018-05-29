085b741013fc170f8bc84c066ebc3cb44de93e20
Evan <evan_24plant@hotmail.com>
2018-05-29 16:09:16 -0700

    Created base login.php page.

---
+++ web/login.php
@@ -0,0 +1,30 @@
+<?php
+include('header.html');
+	
+?>
+
+<script>
+    function validation() {
+        
+    }
+</script>
+
+<article>
+    <div class="textBack" align="center" style="float:left" >
+        <div id="login" align="left">
+            <h1>Log In</h1><br><br><br>
+            <form>
+                <p>Email: <input type="text"/></p>
+                <p>Password: <input type="password"/></p>
+            </form>
+        </div>
+        <div id="signup" align="left">
+            <h1>Sign Up</h1><br><br><br>
+                <p>Don't have an account? <a href="signup.php">Sign up here!</a></p>
+        </div>
+    </div>
+</article>
+
+<?php 
+include "footer.php";
+?>
--- web/template.php
+++ web/template.php
@@ -1,6 +1,6 @@
 <?php
 include('header.html');
-	
+
 ?>
 
 <br>
@@ -12,23 +12,23 @@ include('header.html');
 <br>
 <br>
 
-Lorem ipsum dolor sit amet, consectetuer adipiscing 
-elit. Aenean commodo ligula eget dolor. Aenean massa 
-<strong>strong</strong>. Cum sociis natoque penatibus 
-et magnis dis parturient montes, nascetur ridiculus 
-mus. Donec quam felis, ultricies nec, pellentesque 
-eu, pretium quis, sem. Nulla consequat massa quis 
-enim. Donec pede justo, fringilla vel, aliquet nec, 
-vulputate eget, arcu. In enim justo, rhoncus ut, 
-imperdiet a, venenatis vitae, justo. Nullam dictum 
-felis eu pede <a class="external ext" href="products.php">link</a> 
-mollis pretium. Integer tincidunt. Cras dapibus. 
-Vivamus elementum semper nisi. Aenean vulputate 
-eleifend tellus. Aenean leo ligula, porttitor eu, 
-consequat vitae, eleifend ac, enim. Aliquam lorem ante, 
-dapibus in, viverra quis, feugiat a, tellus. Phasellus 
-viverra nulla ut metus varius laoreet. Quisque rutrum. 
-Aenean imperdiet. Etiam ultricies nisi vel augue. 
+Lorem ipsum dolor sit amet, consectetuer adipiscing
+elit. Aenean commodo ligula eget dolor. Aenean massa
+<strong>strong</strong>. Cum sociis natoque penatibus
+et magnis dis parturient montes, nascetur ridiculus
+mus. Donec quam felis, ultricies nec, pellentesque
+eu, pretium quis, sem. Nulla consequat massa quis
+enim. Donec pede justo, fringilla vel, aliquet nec,
+vulputate eget, arcu. In enim justo, rhoncus ut,
+imperdiet a, venenatis vitae, justo. Nullam dictum
+felis eu pede <a class="external ext" href="products.php">link</a>
+mollis pretium. Integer tincidunt. Cras dapibus.
+Vivamus elementum semper nisi. Aenean vulputate
+eleifend tellus. Aenean leo ligula, porttitor eu,
+consequat vitae, eleifend ac, enim. Aliquam lorem ante,
+dapibus in, viverra quis, feugiat a, tellus. Phasellus
+viverra nulla ut metus varius laoreet. Quisque rutrum.
+Aenean imperdiet. Etiam ultricies nisi vel augue.
 Curabitur ullamcorper ultricies nisi.
 
 </p>
@@ -44,10 +44,10 @@ Curabitur ullamcorper ultricies nisi.
 <br>
 <br>
 
-	
+
 
 </article>
 
-<?php 
+<?php
 include "footer.php";
 ?>
