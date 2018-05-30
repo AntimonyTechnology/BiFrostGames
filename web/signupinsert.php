54622833f2dd9f7190f09ad7d7b5c8446fc56216
Devan Frederick Weber <C0354706@intra.camosun.bc.ca>
2018-05-30 15:07:41 -0700

    Registration page working and updated productInsert.php to accommodate new header

---
+++ web/signupinsert.php
@@ -0,0 +1,48 @@
+<?php
+    session_start();
+    include('header.html');
+    include('connectionSQL.php');
+	
+	echo "<article>";
+    echo "<div class=\"textBack\" align=\"left\" style=\"float:left\">";
+    echo "<h1>Registration</h1><br><br><br>";
+	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+		$fname = strip_tags($_POST['fname']);
+		$lname = strip_tags($_POST['lname']);
+		$email = strip_tags($_POST['email']);
+		$pass = password_hash($_POST['pass'], PASSWORD_DEFAULT);
+		
+		$emailcheckquery = "SELECT * FROM users WHERE email = '$email'";
+		$emailcheck = mysqli_num_rows(@mysqli_query($link, $emailcheckquery));
+		
+		if($emailcheck > 0){
+			echo '<p style="color: red;">Sorry! This email is already in use!<br> <a href="signup.php"> Please try again</a></p>';
+			
+		}else{
+			$adduserquery = "INSERT INTO users (fname, lname, email, password) VALUES ('$fname', '$lname', '$email', '$pass')";
+			@mysqli_query($link, $adduserquery);
+			$emailcheck = mysqli_num_rows(@mysqli_query($link, $emailcheckquery));
+			echo "$emailcheck";
+			if ($emailcheck == 1) {
+				echo "Sign up successful! Please log in <a href =\"login.php\">HERE</a>!";
+			}else{
+				echo "Sign up failed! Please contact the ADMIN with this error : SQL FAILED INSERT!";
+			}
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	echo "</div>";
+    echo "</article>";
+	
+	?>
+<?php
+include('footer.php');
+	
+?>
