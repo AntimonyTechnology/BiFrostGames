1f2266b608862a8e8dab63a9ac53f0c1a55a1137
Curtis Naples <curtis_fanning@hotmail.com>
2018-05-31 15:20:10 -0700

    retry commit that failed

--- web/cart.php
+++ web/cart.php
@@ -9,8 +9,6 @@ include('header.php');
 <form class = "genreSelect" action="receiptInsert.php" method="Post">
 <div class="textBack" align="left" style="float:top" >
 
-<p>
-
 <h1>Cart</h1>
 <br>
 <br>
@@ -24,16 +22,16 @@ include('connectionSQL.php');
 if(isset($_GET['gameId'])) {
 	$gameId = $_GET['gameId'];
 	$userId = 1;	//TEST VALUE change to $_SESSION later
-	$quantity = 1;		//INITIAL INSERT VALUE -- implement the number input box to do update query on this in the value.
+	$quantityDefault = 1;		//INITIAL INSERT VALUE -- implement the number input box to do update query on this in the value.
 
-	$addToCartQuery = "INSERT INTO shopping_cart (user_id, game_id, quantity) VALUES ('$userId', '$gameId', '$quantity')";
+	$addToCartQuery = "INSERT INTO shopping_cart (user_id, game_id, quantity) VALUES ('$userId', '$gameId', '$quantityDefault')";
 	@mysqli_query($link, $addToCartQuery);
 }
 $currGameId=0;
 //use to get which game_id(s) and theyre quantities from shopping cart to store in variables based on userId
 $getCartQuery = 'SELECT * FROM shopping_cart inner join games using (game_id) where user_id = ' . '"' . $userId . '"';
 //remove from cart button query
-$removeCartQuery = 'DELETE FROM shopping_cart WHERE  user_id = ' . '"' . $userId . '"' . ' and game_id = ' . '"' . $currGameId . '"';
+
 //print "remove cart query: " . $removeCartQuery;
 $result = mysqli_query($link, $getCartQuery);
  if ($result)   {
@@ -42,26 +40,40 @@ $result = mysqli_query($link, $getCartQuery);
      
      while ($row = mysqli_fetch_array($result)) {
          //print $row['name'] . '<br>' .
-        $price = $row['price'] * $row['quantity'];
+      $quantity = $row['quantity'];
+      $currGameId = $row['game_id'];
+        $price = $row['price'] * $quantity;
           print '<div class="clearfix">' . 
           '<img class =' . '"' . 'images' . '"' . 'src =' . '"' . $row['image'] . '"><p class="gameName">' . $row['name'] . '</p><br>' .
           '<span class="consoleName">' . $row['console_name'] . '<br>' .
-           '</span><br>' . '<br><br>$'. $price .'<input style=float:right;  type="number" value=' . '"' . $row['quantity'] . '"' . 'name="quantity"></p></div>';
+           '</span><br>' . '<br><br>$'. $price .'<form method="get" action=""><input style=float:right;  type="textbox" value=' . '"' . $row['quantity'] . '"' . 'name="quantity"><input style=float:right; type="button" value="+" onclick="addQ($quantity)"><input style=float:right; type="button" value="-" onclick="remQ($quantity)"><input type="button" value="Remove" onclick="remove($currGameId,$userId)" ></form></p></div>';
          echo '<hr name = "productLine">';
          $totalPrice = $totalPrice + $price;
-         
      }
      
  }
-
+// edit cart quantity
+function addQ($quantity){
+    print "Before addQ: " . $quantity . "<br>";
+    $quantity = $quantity + 1;
+    print "After addQ: " . $quantity . "<br>";
+    return $quantity;
+}
+function remQ($quantity){
+  $quantity = $quantity - 1;
+  return $quantity;
+}
+//remove from cart
+function remove($gameId,$userId){
+  include ('connectionSQL.php');
+  $removeCartQuery = 'DELETE FROM shopping_cart WHERE  user_id = ' . '"' . $userId . '"' . ' and game_id = ' . '"' . $gameId . '"';
+  @mysqli_query($link, $removeCartQuery);
+}
 
 echo $totalPrice . '$';
 ?>
 
 <input type="button" name="checkout" value="Checkout" style="float: right">
-
-
-</p>
 
 </div>
 </form>
