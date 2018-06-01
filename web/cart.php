37ae8133c81df3843530882e7b8601df5b1f5fe7
Curtis Naples <curtis_fanning@hotmail.com>
2018-06-01 13:39:27 -0700

    changed JS functions to use gameId instead of a count to maintain accuracy between pages

--- web/cart.php
+++ web/cart.php
@@ -30,6 +30,18 @@ if(isset($_GET['removeId'])) {
 }
 if(isset($_GET['gameId'])) {
 	$gameId = $_GET['gameId'];
+	$getCartQuery = 'SELECT * FROM shopping_cart inner join games using (game_id) where user_id = ' . '"' . $userId . '"';
+$result = mysqli_query($link, $getCartQuery);
+ if ($result)   {
+     $row_count = mysqli_num_rows($result);
+     //print 'Retreived '. $row_count . ' rows from the <b> games </b> table<BR><BR>';
+     $count = 0;
+     $quantity = 1;
+     while ($row = mysqli_fetch_array($result)) {
+         if($result == $gameId){
+      $gameDuplicate = $row['game_id'];
+  }
+}}
 		//TEST VALUE change to $_SESSION later
 	$quantityDefault = 1;		//INITIAL INSERT VALUE -- implement the number input box to do update query on this in the value.
 
@@ -56,47 +68,51 @@ $result = mysqli_query($link, $getCartQuery);
           '<img class =' . '"' . 'images' . '"' . 'src =' . '"' . $row['image'] . '">
           <p class="gameName">' . $row['name'] . '</p><br>' .
           '<span class="consoleName">' . $row['console_name'] . '<br>' .'</span><br>' . '<br><br>
-          <input type="text box" id=' .'"' . 'price'. $count .'"'.' value ='.'"'. $price . '"'. '>
-          <input id=' .'"' . 'quantity'. $count .'"'. 'style=float:right;  type="textbox" value=1>
-          <input style=float:right; type="button" value="+" onclick='.'"'.'addQ('.$count.','.$price.')"'. '>
-          <input style=float:right; type="button" value="-" onclick='.'"'.'remQ('.$count.','.$price.')"'. '>
+          <input type="text box" id=' .'"' . 'price'. $currGameId .'"'.' value ='.'"'. $price . '"'. '>
+          <input id=' .'"' . 'quantity'. $currGameId .'"'. 'style=float:right;  type="textbox" value=1>
+          <input style=float:right; type="button" value="+" onclick='.'"'.'addQ('.$currGameId.','.$price.')"'. '>
+          <input style=float:right; type="button" value="-" onclick='.'"'.'remQ('.$currGameId.','.$price.')"'. '>
           <input type="submit" value="Remove"></form></div>';
          echo '<hr name = "productLine">';
          $totalPrice = $totalPrice + $price;
+         $gameArray[$count]=$row['game_id'];
          $count = $count + 1;
+         
      }
      
  }
 $_GET['count'] = $count;
 
 $quantityQuery = 'UPDATE shopping_cart set quantity =' . $quantity .   'where game_id ='. $gameId .  'and user_id=' . $userId;
-
+//echo json_encode($gameArray);
 ?>
-
+<input id="total" style=float:right;  type="textbox"><br><br>
 <script>//to change the quantity and individual prices
 	function addQ(count,price) {
 		var quantity = document.getElementById('quantity'+ count).value;
 		quantity++;
 		document.getElementById('quantity'+count).value = quantity;
 		 document.getElementById('price' + count).value = (price * quantity).toFixed(2);
-		 calcTotal(<?php echo $_GET['count'] ?>);
+		 calcTotal(<?php echo json_encode($gameArray); ?>);
 	}
 	function remQ(count,price) {
 		var quantity = document.getElementById('quantity'+ count).value;
 		quantity--;
 		document.getElementById('quantity'+count).value = quantity;
 		 document.getElementById('price' + count).value = (price * quantity).toFixed(2);
-		 calcTotal(<?php echo $_GET['count'] ?>);
+		 calcTotal(<?php echo json_encode($gameArray); ?>);
 	}
-	function calcTotal(count){
+	function calcTotal(gameArray){
+		console.log(gameArray[0]);
 		var total = 0;
-		for (var i = 0; i < count; i++) {
-			total = total + Number(document.getElementById('price' + i).value);
+		for (var i = 0; i < gameArray.length; i++) {
+			total = total + Number(document.getElementById('price' + gameArray[i]).value);
 		}
-		document.getElementById('total').value = total;
+		document.getElementById('total').value = total.toFixed(2);
 	}
+	calcTotal(<?php echo json_encode($gameArray); ?>);
 </script>
-<input id="total" style=float:right;  type="textbox"><script>calcTotal(<?php echo $_GET['count'] ?>);</script><br><br>
+
 <input type="submit" name="checkout" value="Checkout" style="float: right">
 
 </div>
