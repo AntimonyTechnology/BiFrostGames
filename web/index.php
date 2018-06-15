6b70d6808afcba8218a3a1a6c3a4a7fb96f74b31
Curtis Naples <curtis_fanning@hotmail.com>
2018-06-14 18:11:47 -0700

    flapping bird on the home pagegit add .!

--- web/index.php
+++ web/index.php
@@ -13,25 +13,19 @@ include('header.php');
 <br>
 <br>
 <br>
+ <a class="external ext" href="products.php">
+<div class="container">
+	
+	<div class="bird-container">
+		<div class="bird"></div>
+	</div>
 
-Lorem ipsum dolor sit amet, consectetuer adipiscing 
-elit. Aenean commodo ligula eget dolor. Aenean massa 
-<strong>strong</strong>. Cum sociis natoque penatibus 
-et magnis dis parturient montes, nascetur ridiculus 
-mus. Donec quam felis, ultricies nec, pellentesque 
-eu, pretium quis, sem. Nulla consequat massa quis 
-enim. Donec pede justo, fringilla vel, aliquet nec, 
-vulputate eget, arcu. In enim justo, rhoncus ut, 
-imperdiet a, venenatis vitae, justo. Nullam dictum 
-felis eu pede <a class="external ext" href="products.php">link</a> 
-mollis pretium. Integer tincidunt. Cras dapibus. 
-Vivamus elementum semper nisi. Aenean vulputate 
-eleifend tellus. Aenean leo ligula, porttitor eu, 
-consequat vitae, eleifend ac, enim. Aliquam lorem ante, 
-dapibus in, viverra quis, feugiat a, tellus. Phasellus 
-viverra nulla ut metus varius laoreet. Quisque rutrum. 
-Aenean imperdiet. Etiam ultricies nisi vel augue. 
-Curabitur ullamcorper ultricies nisi.
+</div>
+<br>
+<center><div class="gameName">Go to Products</div></center>
+</a> 
+<br>
+
 
 </p>
 </div>
